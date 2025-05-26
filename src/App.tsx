2e65402640ad4eb12bd88cce8a07f936ac5cac65
young <canbbu222@gmail.com>
2025-05-26 19:04:06 +0900

    deploy lol

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
         <AuthProvider>
           <Routes>
             {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route element={<RequireAuth />}>
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/register" element={<Register />} />
